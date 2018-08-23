c019961d7336c14693f4d4f0e756f0062801a2d9
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2018-08-23 17:01:46 +0100

    Initial commit

---
+++ Source/Pasithea/PasitheaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PasitheaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PASITHEA_API APasitheaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
